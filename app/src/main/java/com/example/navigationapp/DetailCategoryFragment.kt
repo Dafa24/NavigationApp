30c97220f3644950bac4febf10e753651f63f737
Dafa24 <akbarpradipa@gmail.com>
2020-11-20 04:34:10 +0700

    first commit'

---
+++ app/src/main/java/com/example/navigationapp/DetailCategoryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigationapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.detail_category_fragment.*
+
+class DetailCategoryFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+        tv_category_name.setText(dataName)
+        tv_category_description.setText("Stock : $dataDescription")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.detail_category_fragment, container, false)
+    }
+ }
